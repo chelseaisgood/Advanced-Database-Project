a7d838f962bd65885be9f57778edefab6211d254
chelseaisgood <chelseaisgoodfmd@gmail.com>
2016-10-31 17:00:28 -0400

    2016-10-31 5pm updates

--- src/edu/nyu/csciga2434/project/Site.java
+++ src/edu/nyu/csciga2434/project/Site.java
@@ -8,12 +8,14 @@ import java.util.*;
  * Time: 3:14 PM
  *
  * There are total 10 sites, numbered 1 to 10.
+ * Odd indexed variable are at one site each(i.e. 1+ index # mod 10). Even indexed variables are at all sites.
  * Every site has a lock table and when the site fails, the lock table is erased.
  */
 
 public class Site {
 
     private static final int DEFAULT_SITE_TOTAL_NUMBER = 10;
+    private static final int DEFAULT_VARIABLE_TOTAL_NUMBER = 20;
 
     private final int id;
     private List<Variable> variableList;
@@ -24,12 +26,13 @@ public class Site {
         this.id = id;
         this.variableList = new ArrayList<>();
         this.lockTable = new LockTable();
-        this.ifSiteWorking = true;
         BuildingSite();
+        this.ifSiteWorking = true;
     }
 
     private void BuildingSite(){
-        for (int i = 1; i <= DEFAULT_SITE_TOTAL_NUMBER; i++){
+        // call BuildingSite() when the site is first set and include all the variables that should be initially held by this site.
+        for (int i = 1; i <= DEFAULT_VARIABLE_TOTAL_NUMBER; i++){
             if(i % 2 == 0){
                 this.variableList.add(new Variable(i));
             }else{
